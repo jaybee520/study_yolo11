7adcf3fea502f4650d4df4521cd1b631e06dccb0
jaybee520 <neast-z@foxmail.com>
2024-10-13 01:21:06 +0800

    add example

---
+++ Keypoints.py
@@ -0,0 +1,26 @@
+from ultralytics import YOLO
+import cv2
+from PIL import Image
+
+MODEL = 'yolo11s-pose.pt'
+VAL_IMG = 'images/original.jpeg'
+
+# Load a COCO-pretrained YOLO11n model
+model = YOLO(MODEL)
+
+# Train the model on the COCO8 example dataset for 100 epochs
+# results = model. train(data="coco8.yaml", epochs=100, imgsz=640)
+
+# Run inference with the YOLO11n model on the 'bus.jpg' image
+results = model(VAL_IMG)
+
+# from ndarray
+im2 = cv2.imread(VAL_IMG)
+results = model.predict(source=im2, save=True, save_txt=True)  # save predictions as labels
+
+# from list of PIL/ndarray
+results = model.predict(source=[ im2])
+
+# View results
+for r in results:
+    print(r.keypoints) 
